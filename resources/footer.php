f424a65c283b7029be117f051d6d266899dacd4e
MADAVAN K <kmadavan018@gmail.com>
2023-10-26 20:08:22 +0530

    footer dynamic

---
+++ resources/footer.php
@@ -0,0 +1,113 @@
+<footer class="footer-section">
+    <div class="container footer-top">
+        <div class="row">
+            <div class="col-sm-6 col-lg-3 footer-widget">
+                <div class="about-widget">
+                    <img src="img/logo-light.png" alt />
+                    <p>
+                        orem ipsum dolor sit amet, consecter adipiscing elite. Donec
+                        minos varius, viverra justo ut, aliquet nisl.
+                    </p>
+                    <div class="social pt-1">
+                        <a href><i class="fa fa-twitter-square"></i></a>
+                        <a href><i class="fa fa-facebook-square"></i></a>
+                        <a href><i class="fa fa-google-plus-square"></i></a>
+                        <a href><i class="fa fa-linkedin-square"></i></a>
+                        <a href><i class="fa fa-rss-square"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-3 footer-widget">
+                <h6 class="fw-title">USEFUL LINK</h6>
+                <div class="dobule-link">
+                    <ul>
+                        <li><a href>Home</a></li>
+                        <li><a href>About us</a></li>
+                        <li><a href>Services</a></li>
+                        <li><a href>Events</a></li>
+                        <li><a href>Features</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href>Policy</a></li>
+                        <li><a href>Term</a></li>
+                        <li><a href>Help</a></li>
+                        <li><a href>FAQs</a></li>
+                        <li><a href>Site map</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-3 footer-widget">
+                <h6 class="fw-title">RECENT POST</h6>
+                <ul class="recent-post">
+                    <li>
+                        <p>
+                            Snackable study:How to break <br />
+                            up your master's degree
+                        </p>
+                        <span><i class="fa fa-clock-o"></i>24 Mar 2018</span>
+                    </li>
+                    <li>
+                        <p>
+                            Open University plans major <br />
+                            cuts to number of staff
+                        </p>
+                        <span><i class="fa fa-clock-o"></i>24 Mar 2018</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-sm-6 col-lg-3 footer-widget">
+                <h6 class="fw-title">CONTACT</h6>
+                <ul class="contact">
+                    <li>
+                        <p>
+                            <i class="fa fa-map-marker"></i> 40 Baria Street 133/2,
+                            NewYork City,US
+                        </p>
+                    </li>
+                    <li>
+                        <p><i class="fa fa-phone"></i> (+88) 111 555 666</p>
+                    </li>
+                    <li>
+                        <p>
+                            <i class="fa fa-envelope"></i>
+                            <a href="/cdn-cgi/l/email-protection" class="__cf_email__"
+                                data-cfemail="335a5d555c5756564150415652475a455673545e525a5f1d505c5e">[email&#160;protected]</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <i class="fa fa-clock-o"></i> Monday - Friday, 08:00AM - 06:00
+                            PM
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright container" style="display: flex;justify-content: space-between;">
+        <div style="filter: drop-shadow(0px 0px 30px rgba(255, 255, 255, 0.7));">
+            <img width="100px" src="https://nscet.org/waves23/public/images/logos/ispin-logo.png" alt="" srcset="">
+        </div>
+        <div class="container">
+            <p>
+                Copyright &copy;
+                <script data-cfasync="false"
+                    src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+                <script>
+                    document.write(new Date().getFullYear());
+                </script>
+                All rights reserved. Design & Developed with <i style="color: red;" class="fa fa-heart"></i> by,
+                <br />
+                <span style="color: tomato;">Innovative Software Product Industry of NSCET </span>
+                <br />Department of Computer Science Engineering
+            </p>
+        </div>
+        <div style="filter: drop-shadow(0px 0px 30px rgba(255, 255, 255, 0.7));">
+            <img width="150px" src="https://nscet.org/waves23/public/images/logos/clg-logo.png" alt="" srcset="">
+        </div>
+    </div>
+</footer>
