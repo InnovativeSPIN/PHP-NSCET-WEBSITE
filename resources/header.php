266d668328a8b5a2db97801cb675f5c3d482ea06
MUKILAN S <mukilan069@gmail.com>
2023-10-29 21:35:49 +0530

    Header COnent Navigaton change

--- resources/header.php
+++ resources/header.php
@@ -1,16 +1,25 @@
 <nav class="nav-section">
     <div class="container">
         <ul class="main-menu">
-            <a href="index.html" class="site-logo"><img width="125px"
-                    src="https://nscet.org/Home_Assets/img/nscet/College%20logo.png" alt /></a>
+            <a href="index.html" class="site-logo"><img width="125px" src="https://nscet.org/Home_Assets/img/nscet/College%20logo.png" alt /></a>
             <li class="active"><a href="index.html">Home</a></li>
             <li><a href="about.html">Administartion</a></li>
             <li><a href="#">Admission</a></li>
             <li><a href="course.html">Acadamic</a></li>
             <li>
                 <div class="dropdown custome-dropdowm">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Faculty
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Teaching Faculty</a>
+                        <a class="dropdown-item" href="#">Non-Teaching Faculty</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="dropdown custome-dropdowm">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Department
                     </button>
                     <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -24,24 +33,64 @@
             <li><a href="course.html">Library</a></li>
             <li><a href="blog.html">MOOC</a></li>
             <li><a href="contact.html">Placeent</a></li>
-
-            <li class="active">
+            <!-- <li class="active">
                 <a href><i class="fa fa-user"> Login </i></a>
-            </li>
+            </li> -->
         </ul>
         <ul class="main-menu" style="padding-bottom: 12px">
-            <li><a href="index.html">Conference</a></li>
-            <li><a href="about.html">Welefare Cell</a></li>
+            <li>
+                <div class="dropdown custome-dropdowm">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Conference
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">ICAMS'23</a>
+                        <a class="dropdown-item" href="#">ICNSCET'23 - Procedding Book</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="dropdown custome-dropdowm">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Welefare Cell
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="">SC/ST Welfare Cell</a>
+                        <a class="dropdown-item" href="">Anti Ragging</a>
+                        <a class="dropdown-item" href=''>Cells & Clubs</a>
+                        <a class="dropdown-item" href=''>Anti Ragging Squad</a>
+                        <a class="dropdown-item" href=''>Internal Complaints</a>
+                        <a class="dropdown-item" href=''>Grievance Redressal</a>
+                    </div>
+                </div>
+            </li>
             <li><a href="#">Infrastructure</a></li>
             <li><a href="course.html">Galary</a></li>
             <li><a href="blog.html">Career</a></li>
             <li><a href="contact.html">Alumini</a></li>
-            <li><a href="course.html">Clubs & Chapters</a></li>
-            <li><a href="blog.html">NIRF</a></li>
+            <li>
+                <div class="dropdown custome-dropdowm">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Clubs & Chapters
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="">Updated Soon !!</a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="dropdown custome-dropdowm">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        NIRF
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="">Updated Soon !!</a>
+                        <a class="dropdown-item" href="">Updated Soon !!</a>
+                    </div>
+                </div>
+            </li>
             <li><a href="contact.html">IQAS</a></li>
             <li><a href="contact.html">Research</a></li>
-
-
         </ul>
     </div>
 </nav>
