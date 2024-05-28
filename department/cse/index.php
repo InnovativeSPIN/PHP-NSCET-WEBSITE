732b4aff8c283256a62109db49cb42d4cf426925
B.NAVEEN BHARATHI <144258519+bnaveenbharathi@users.noreply.github.com>
2024-05-28 20:20:49 +0530

    Structure and Style update

--- department/cse/index.php
+++ department/cse/index.php
@@ -29,6 +29,22 @@ require_once("../../resources/connection.php") ?>
   <link rel="stylesheet" href="../../css/departmentPages.css" />
 
   <script nonce="defdf876-aff2-4c43-8786-bc6a2f07dfb1">
+
+document.addEventListener('DOMContentLoaded', function() {
+  const iconElement = document.querySelector('.sectiontongglelink .fa-solid.fa-caret-right');
+  const sectionBar = document.querySelector('.navigation-bar');
+
+  iconElement.addEventListener('click', function() {
+    if (sectionBar.style.display === 'none') {
+      sectionBar.style.display = 'flex';
+    } else {
+      sectionBar.style.display = 'none';
+    
+    }
+  });
+});
+
+
     (function(w, d) {
       !(function(t, u, v, w) {
         t[v] = t[v] || {};
@@ -108,6 +124,10 @@ require_once("../../resources/connection.php") ?>
 <body>
 
   <div id="fixed-social" style="z-index: 10">
+  <div class="sectiontongglelink">
+  <i class="fa-solid fa-caret-right"></i>
+    </div>
+    
     <div>
       <a href="#" class="fixed-facebook" target="_blank"><i class="fa fa-facebook"></i> <span>Facebook</span></a>
     </div>
@@ -120,19 +140,21 @@ require_once("../../resources/connection.php") ?>
     <div>
       <a href="#" class="fixed-twitter" target="_blank"><i class="fa fa-instagram"></i> <span>Instagram</span></a>
     </div>
+   
   </div>
 
   <div class="nav-switch" style="z-index: 100">
     <i class="fa fa-bars"></i>
+    
   </div>
 
   <?php require_once("../../resources/header.php") ?>
 
     <section class="hero-section">
-      <h1 class="dept-heading"><strong>Department of Computer Science Engineering</strong></h1>
-      
+    <h1 class="dept-heading"><strong>Department of Computer Science Engineering</strong></h1>
     </section>
-
+   
+    
     <section class="navigation-bar">
       <nav class="page__menu menu">
         <ul class="menu__list r-list">
@@ -145,35 +167,37 @@ require_once("../../resources/connection.php") ?>
         </ul>
       </nav>
     </section>
+    <!-- reposive icon -->
+   
+
 
     <div class="dept-page-content">
     <!-- our dept section -->
     <section class="our-dept" id="about">
+      <div class="section111">
       <div class="text-sec">
-        <h1 class="dept-title text-center mb-4" style="margin-top:3% ;">OUR DEPARTMENT</h1>
+        <h1 class="dept-title text-center mb-4" style="margin-top:3% ;color:#104565;">OUR DEPARTMENT</h1>
         <div class="border-line"></div>
         <h4 class="dept-sub-title"> The Department of Computer Science and Engineering aims to produce the engineers with the abilities:</h4>
         <ul class="dept-sub-con">
-          <li> Be lifelong learners who continue to pursue professional development.</li>
-          <li> Participate and thrive in a multi disciplinary, systems oriented work environment.</li>
-          <li> Contribute to the solution of complex technical problems that exist in the software industry.</li>
-          <li>Understand their ethical roles as a professional engineer and strive to promote a practice of integrity, tolerance and respect in the work place.<br>
-          </li>
+          <li>Be lifelong learners who continue to pursue professional development.</li>
+          <li>Participate and thrive in a multi disciplinary, systems oriented work environment.</li>
+          <li>Contribute to the solution of complex technical problems that exist in the software industry.</li>
+          <li>Understand their ethical roles as a professional engineer and strive to promote a practice of integrity, tolerance and respect in the work place.</li>
         </ul>
       </div>
-
       <div class="container custom-card-container">
         <div class="custom-card-wrapper" style="background-image: url('../../img/department/cse/vision.png');">
           <div class="custom-card-overlay"></div>
-          <div class="custom-card-content">
+          <div class="custom-card-content " style="height: 40vh;">
             <h4 class="custom-card-title"><strong>VISION</strong></h4>
             <p class="custom-card-text">To become a leading hub in the field of Computer Engineering.</p>
           </div>
         </div>
-
+<br><br>
         <div class="custom-card-wrapper" style="background-image: url('../../img/department/cse/our-mission.png');">
           <div class="custom-card-overlay"></div>
-          <div class="custom-card-content">
+          <div class=" custom-card-content custom-card-content2 " style="height: 230px;">
             <h4 class="custom-card-title"><strong>MISSION</strong></h4>
             <p style="text-align: left;" class="custom-card-text">
               • To provide a strong theoretical and practical knowledge emphasizing on software developments. </br>
@@ -183,131 +207,12 @@ require_once("../../resources/connection.php") ?>
           </div>
         </div>
       </div>
+     
+      </div>
     </section>
+<!-- PURSUITS section -->
 
-    <!-- hod section -->
-
-    <div id="hod_desk">
-      <div class="container">
-        <div class="hod-section">
-          <h1 class="section-title text-center mb-4">HEAD OF DEPARTMENT</h1>
-          <div class="border-line" style="margin-bottom:5%;"></div>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="hod-image text-center">
-                <img src="../../img/department/cse/Mathalairaj.jpg" alt="Mr. J. Mathalai Raj" class="img-fluid rounded-circle">
-                <h5 class="text-uppercase mt-3">Mr. J. Mathalai Raj</h5>
-              </div>
-            </div>
-            <div class="col-md-6 d-flex align-items-center">
-              <div class="hod-description">
-                <p class="text-justify">The Computer Science and Engineering department aims at nurturing a conducive upbringing for erudition and research by using appropriate computing technologies in its everyday activities. The department equips its students with technological expertise and skills in the field of computer science. Students at CSE are nurtured to become the best software professionals or entrepreneurs in their own innovative way. The prime objective of the department endeavors is to produce confident professionals tuned to real-time working environments. The department offers an excellent academic environment with a team of highly qualified faculty members to inspire the students to develop their technical skills and inculcate the spirit of teamwork in them.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- FACILITIES section -->
-
-    <div id="facilities" style="margin-bottom: 3%;">
-
-      <h1 class="section-title text-center mb-4">FACILITIES</h1>
-      <div class="border-line" style="margin-bottom:5%;"></div>
-
-      <div class="container">
-        <main class="grid">
-          <article>
-            <img src="../../img/department/cse/facilities_section/1.jpg" alt="Dennis lab">
-            <i class="fa-solid fa-computer"></i>
-            <div class="text">
-              <h3 class="facility-heading"><strong>Dennis Laboratory</strong></h3>
-              <p>Programming Lab which uses C and C++ Languages for Basic Programming.</p>
-            </div>
-          </article>
-          <article>
-            <img src="../../img/department/cse/facilities_section/2.jpg" alt="Bachman lab">
-            <i class="fa-solid fa-computer-mouse"></i>
-            <div class="text">
-              <h3 class="facility-heading"><strong>Bachman Laboratory</strong></h3>
-              <p>Data Design Lab which uses C,C++,Visual Studio,JAVA,Oracle ,SQL ,Rational Suite for developing and implementing projects..</p>
-            </div>
-          </article>
-          <article>
-            <img src="../../img/department/cse/facilities_section/3.jpg" alt="Linus lab">
-            <i class="fa-solid fa-network-wired"></i>
-            <div class="text">
-              <h3 class="facility-heading"><strong>Linus Torvalds Laboratory</strong></h3>
-              <p>Open Source Lab which uses C,C++,Android Studio,Eclipse,Maya,OPENGL MC for developing and implementing projects.</p>
-            </div>
-          </article>
-          <article>
-            <img src="../../img/department/cse/facilities_section/4.jpg" alt="Computer centre">
-            <i class="fa-solid fa-server"></i>
-            <div class="text">
-              <h3 class="facility-heading"><strong>Computer Centre</strong></h3>
-              <p>A Programming Centre which uses C,C++,VB,VC++,Java Languages for Basic Programming.</p>
-            </div>
-          </article>
-          <article>
-            <img src="../../img/department/cse/facilities_section/5.jpg" alt="Project lab">
-            <i class="fa-solid fa-robot"></i>
-            <div class="text">
-              <h3 class="facility-heading"><strong>Project Laboratory</strong></h3>
-              <p>An Exclusive Project Lab for developing Innovative Projects using Java,SQL,VB,VC++,Net Beans,.NET.</p>
-            </div>
-          </article>
-        </main>
-      </div>
-    </div>
-
-    <!-- ORGANIZATION section -->
-    <div id="organization">
-      <h1 class="section-title text-center mb-4">ORGANIZATION</h1>
-      <div class="border-line" style="margin-bottom:5%;"></div>
-
-      <div class="container org-container">
-        <div class="row">
-          <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="dept-org-link floating-image">
-              <a href="https://nscet.org/ispin">
-                <img src="../../img/department/cse/ispin_log.png" alt="iSPIN">
-                <h1>iSPIN</h1>
-              </a>
-            </div>
-          </div>
-
-          <div class="col-lg-6 col-md-6 col-sm-12">
-            <div class="dept-org-link floating-image">
-              <a href="https://nscet.org/associations/fist.html">
-                <img src="../../img/department/cse/fist.jpg" alt="FIST">
-                <h1>FIST</h1>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="org-content">
-        <div class="org-con-section">
-          <h1 class="org-con-side-head">&nbsp;Innovative Software Product Industry of NSCET&nbsp;</h1>
-          <p class="org-con-paragraph">iSPIN is a pure IT sector of NSCET with Resourceful students under the guidance of Faculties, Industry Specialists, and the Alumni of NSCET. It will be the First of its kind idea for the incubation cell of NSCET. iSPIN will be responsible for developing the products for NSCET and its clients. The essence of iSPIN is the strategy of Unlearn & Learn based on Self-Learning Strategy.</p>
-          <a data-toggle="modal" data-target="#modal-team" type="button" class="org-con-btn">
-            <span>Meet Our Team</span>
-          </a>
-        </div>
-
-        <div class="org-con-section">
-          <h1 class="org-con-side-head">&nbsp;Fraternity of Immortal Software Technocrats</h1>
-          <p class="org-con-paragraph">Never doubt that a group of talented engineers and developers, who love to involve both themselves and others in every technical and non-technical event led to the growth of our proud “COMPUTER SCIENCE ENGINEERING ASSOCIATION”. Our FIST Association has been successfully running over six years. Every year we conduct events that invoke the technical and creative skills of every single individual and with the constant support of all the students, our association has stepped into the 7th year of excellence. Strengthen our association with your pillars of support and we hope for the best of our association in the future.</p>
-        </div>
-      </div>
-    </div>
-
-    <!-- PURSUITS section -->
-
-    <div class="pursuits-sec">
+<div class="pursuits-sec">
       <h1 class="section-title text-center mb-4">PURSUITS</h1>
       <div class="border-line" style="margin-bottom:5%;"></div>
       <div id="pursuits" class="carousel slide  vertical" data-ride="carousel" style="padding:20px;margin-left:20px;margin-right:20px;">
@@ -367,6 +272,132 @@ require_once("../../resources/connection.php") ?>
         </div>
       </div>
     </div>
+    <!-- hod section -->
+
+    <div id="hod_desk">
+      <div class="container">
+        <div class="hod-section">
+          <h1 class="section-title text-center mb-3 " style="color:#104565;">HEAD OF DEPARTMENT</h1>
+          <div class="border-line" style="margin-bottom:5%;"></div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="hod-image text-center">
+                <img src="../../img/department/cse/Mathalairaj.jpg" alt="Mr. J. Mathalai Raj" class="">
+                <h5 class="text-uppercase mt-3">Mr. J. Mathalai Raj</h5>
+              </div>
+            </div>
+            <div class="col-md-6 d-flex align-items-center">
+              <div class="hod-description">
+                <p class="text-justify">The Computer Science and Engineering department aims at nurturing a conducive upbringing for erudition and research by using appropriate computing technologies in its everyday activities. The department equips its students with technological expertise and skills in the field of computer science. Students at CSE are nurtured to become the best software professionals or entrepreneurs in their own innovative way. The prime objective of the department endeavors is to produce confident professionals tuned to real-time working environments. The department offers an excellent academic environment with a team of highly qualified faculty members to inspire the students to develop their technical skills and inculcate the spirit of teamwork in them.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- FACILITIES section -->
+
+    <div id="facilities " style="margin-bottom: 3%; margin-top:3%;">
+
+      <h1 class="section-title text-center mb-4">FACILITIES</h1>
+      <div class="border-line" style="margin-bottom:5%;"></div>
+
+      <div class="container">
+        <main class="grid">
+          <article>
+            <img src="../../img/department/cse/facilities_section/1.jpg" alt="Dennis lab">
+            <i class="fa-solid fa-computer"></i>
+            <div class="text">
+              <h3 class="facility-heading"><strong>Dennis Laboratory</strong></h3>
+              <p>Programming Lab which uses C and C++ Languages for Basic Programming.</p>
+            </div>
+          </article>
+          <article>
+            <img src="../../img/department/cse/facilities_section/2.jpg" alt="Bachman lab">
+            <i class="fa-solid fa-computer-mouse"></i>
+            <div class="text">
+              <h3 class="facility-heading"><strong>Bachman Laboratory</strong></h3>
+              <p>Data Design Lab which uses C,C++,Visual Studio,JAVA,Oracle ,SQL ,Rational Suite for developing and implementing projects..</p>
+            </div>
+          </article>
+          <article>
+            <img src="../../img/department/cse/facilities_section/3.jpg" alt="Linus lab">
+            <i class="fa-solid fa-network-wired"></i>
+            <div class="text">
+              <h3 class="facility-heading"><strong>Linus Torvalds Laboratory</strong></h3>
+              <p>Open Source Lab which uses C,C++,Android Studio,Eclipse,Maya,OPENGL MC for developing and implementing projects.</p>
+            </div>
+          </article>
+          <article>
+            <img src="../../img/department/cse/facilities_section/4.jpg" alt="Computer centre">
+            <i class="fa-solid fa-server"></i>
+            <div class="text">
+              <h3 class="facility-heading"><strong>Computer Centre</strong></h3>
+              <p>A Programming Centre which uses C,C++,VB,VC++,Java Languages for Basic Programming.</p>
+            </div>
+          </article>
+          <article>
+            <img src="../../img/department/cse/facilities_section/5.jpg" alt="Project lab">
+            <i class="fa-solid fa-robot"></i>
+            <div class="text">
+              <h3 class="facility-heading"><strong>Project Laboratory</strong></h3>
+              <p>An Exclusive Project Lab for developing Innovative Projects using Java,SQL,VB,VC++,Net Beans,.NET.</p>
+            </div>
+          </article>
+        </main>
+      </div>
+    </div>
+
+    <!-- ORGANIZATION section -->
+    <div id="organization">
+      <h1 class="section-title text-center mb-2">ORGANIZATION</h1>
+      <div class="border-line  mt-2" style="margin-bottom:2%;"></div>
+
+      <div class="container org-container">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+           
+          </div>
+
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            
+          </div>
+        </div>
+      </div>
+
+      <div class="org-content">
+        <div class="org-con-section ">
+          <h1 class="org-con-side-head">&nbsp;Innovative Software Product Industry of NSCET&nbsp;</h1>
+<div class="org-con-section1">
+          <p class="org-con-paragraph ">iSPIN is a pure IT sector of NSCET with Resourceful students under the guidance of Faculties, Industry Specialists, and the Alumni of NSCET. It will be the First of its kind idea for the incubation cell of NSCET. iSPIN will be responsible for developing the products for NSCET and its clients. The essence of iSPIN is the strategy of Unlearn & Learn based on Self-Learning Strategy.</p>
+          <div class="dept-org-link floating-image">
+              <a href="https://nscet.org/ispin">
+                <img src="../../img/department/cse/ispin_log.png" alt="iSPIN">
+                <h1>iSPIN</h1>
+              </a>
+            </div>
+            </div>
+          <a data-toggle="modal" data-target="#modal-team" type="button" class="org-con-btn">
+            <span>Meet Our Team</span>
+          </a>
+        </div>
+        <div class="org-con-section">
+          <h1 class="org-con-side-head">&nbsp;Fraternity of Immortal Software Technocrats</h1>
+          <div class="org-con-section1">
+          <div class="dept-org-link floating-image">
+              <a href="https://nscet.org/associations/fist.html">
+                <img src="../../img/department/cse/fist.jpg" alt="FIST">
+                <h1>FIST</h1>
+              </a>
+            </div>
+          <p class="org-con-paragraph">Never doubt that a group of talented engineers and developers, who love to involve both themselves and others in every technical and non-technical event led to the growth of our proud “COMPUTER SCIENCE ENGINEERING ASSOCIATION”. Our FIST Association has been successfully running over six years. Every year we conduct events that invoke the technical and creative skills of every single individual and with the constant support of all the students, our association has stepped into the 7th year of excellence. Strengthen our association with your pillars of support and we hope for the best of our association in the future.</p>
+        </div>
+        </div>
+      </div>
+    </div>
+
+    
 
     <!-- OFFICE BEARERS section   -->
 
